f03b9318c13903ea843b135a82cbafcdd1610e11
James-W-LTU <j.woodthorpe03@gmail.com>
2024-12-02 15:46:27 +0000

    completed week 2 tasks

--- Week 2/taskTwo/src/main/testModelEmployee.java
+++ Week 2/taskTwo/src/main/testModelEmployee.java
@@ -1,8 +1,16 @@
-package main;
+import main.modelEmployee;
 
 public class testModelEmployee {
     public static void main(String[] args) {
         modelEmployee employee1 = new modelEmployee("Adam", "Smith", 34, 30000, 99);
-        System.out.println(employee1);
+        modelEmployee employee2 = new modelEmployee("John", "Brown", 52, 38000, 67);
+
+
+        System.out.println("Employee class example");
+
+
+        System.out.println("\nEmployee 1 firstname, surname and age: " + employee1.getFirstname() + " " + employee1.getSurname() + ", " + employee1.getAge())
+        ;
+        System.out.println("\nEmployee 2 firstname, surname and salary: " + employee2.getFirstname() + " " + employee2.getSurname() + ", " + employee2.getSalary());
     }
 }
